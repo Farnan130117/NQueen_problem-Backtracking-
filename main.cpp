1058b24a4729e54eda694389e0d0ca668711ded7
Md. Mokammel Hossen Farnan <mh.farnan@yahoo.com>
2017-01-03 13:17:33 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,74 +1,60 @@
-#include<iostream>
-#include<cstdio>
+#include <iostream>
+#include <cstdio>
 #include<cmath>
 using namespace std;
-int board[20],count;
-
+int board[10],n,count;
 int main()
 {
- int n,i,j;
- void queen(int row,int n);
-
- printf(" - N Queens Problem Using Backtracking -");
- printf("\n\nEnter number of Queens:");
- scanf("%d",&n);
- queen(1,n);
- return 0;
+    int n,i,j;
+    void queen(int row,int n);
+    cin>>n;
+    queen(1,n);
+    return 0;
 }
-
-//function for printing the solution
-void print(int n)
+int print(int n)
 {
- int i,j;
- printf("\n\nSolution %d:\n\n",++count);
-
- for(i=1;i<=n;++i)
-  printf("\t%d",i);
-
- for(i=1;i<=n;++i)
- {
-  printf("\n\n%d",i);
-  for(j=1;j<=n;++j) //for nxn board
-  {
-   if(board[i]==j)
-    printf("\tQ"); //queen at i,j position
-   else
-    printf("\t-"); //empty slot
-  }
- }
+    int i,j;
+    cout<<"\nSolution:"<<endl;
+    for(i=1;i<=n;i++)
+        printf("\t%d",i);
+    for(i=1;i<=n;i++)
+    {
+        printf("\n\n%d\n\n",i);
+        for(j=1;j<=n;j++)
+        {
+            if(board[i]==j)
+                printf("\tQ");
+            else printf("\t-");
+        }
+    }
 }
-
-/*funtion to check conflicts
-If no conflict for desired postion returns 1 otherwise returns 0*/
 int place(int row,int column)
 {
- int i;
- for(i=1;i<=row-1;++i)
- {
-  //checking column and digonal conflicts
-  if(board[i]==column)
-   return 0;
-  else
-   if(abs(board[i]-column)==abs(i-row))
-    return 0;
- }
+    int i;
+    for(i=1;i<=row-1;++i)
+    {
+        if(board[i]==column)
+            return 0;
+        else{
+            if(abs(board[i]-column)==abs(i-row))
+                return 0;
+        }
+    }
+    return 1;
 
- return 1; //no conflicts
 }
-
-//function to check for proper positioning of queen
-void queen(int row,int n)
+int queen(int row,int n)
 {
- int column;
- for(column=1;column<=n;++column)
- {
-  if(place(row,column))
-  {
-   board[row]=column; //no conflicts so place queen
-   if(row==n) //dead end
-    print(n); //printing the board configuration
-   else //try queen with next position
-    queen(row+1,n);
-  }
- }
+    int column;
+    for(column=1;column<=n;++column)
+    {
+        if(place(row,column))
+        {
+           board[row]=column;
+           if(row==n)
+                print(n);
+           else
+            queen(row+1,n);
+        }
+    }
 }
